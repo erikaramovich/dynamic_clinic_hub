a9a6173ae4720695ac957e3a69cae6f68a046639
erikaramovich <erik.g@miro.com>
2026-05-02 02:04:45 +0400

    Auth ms: add @Schema with description and example of request DTOs

--- authentication-ms/src/main/java/com/miro/project/dto/request/LoginRequest.java
+++ authentication-ms/src/main/java/com/miro/project/dto/request/LoginRequest.java
@@ -1,14 +1,17 @@
 package com.miro.project.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class LoginRequest {
 
+    @Schema(description = "Your name", example = "John")
     @NotBlank(message = "Name is required")
     private String name;
 
+    @Schema(description = "Your password", example = "jacksonLS63!+")
     @NotBlank(message = "Password is required")
     private String password;
 }
--- authentication-ms/src/main/java/com/miro/project/dto/request/RegisterRequest.java
+++ authentication-ms/src/main/java/com/miro/project/dto/request/RegisterRequest.java
@@ -1,18 +1,22 @@
 package com.miro.project.dto.request;
 
 import com.miro.project.model.Role;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.*;
 import lombok.Data;
 
 @Data
 public class RegisterRequest {
+    @Schema(description = "Your name", example = "John")
     @NotBlank(message = "Name is required")
     private String name;
 
+    @Schema(description = "Your email", example = "john@miro.com")
     @NotBlank(message = "Email is required")
     @Email(message = "Please provide a valid email address")
     private String email;
 
+    @Schema(description = "Your password", example = "jacksonLS63!+")
     @NotBlank(message = "Password is required")
     @Size(min = 8, max = 20, message = "Password must be between 8 and 20 characters")
     @Pattern(
@@ -22,6 +26,7 @@ public class RegisterRequest {
     private final String password;
 
 
+    @Schema(description = "Your Role in clinic hub", example = "PATIENT")
     @NotNull(message = "Role is required (PATIENT, DOCTOR, ADMINISTRATOR)")
     private Role role;
 }
