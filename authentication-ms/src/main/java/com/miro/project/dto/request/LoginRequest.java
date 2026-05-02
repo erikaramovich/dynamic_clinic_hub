7dde317a8e6b39006d906b3caaaf17112b22fedd
erikaramovich <erik.g@miro.com>
2026-05-02 04:39:35 +0400

    Auth ms: add Role field in LoginRequest to provide login the same person in specific role, which mentions

--- authentication-ms/src/main/java/com/miro/project/dto/request/LoginRequest.java
+++ authentication-ms/src/main/java/com/miro/project/dto/request/LoginRequest.java
@@ -1,7 +1,9 @@
 package com.miro.project.dto.request;
 
+import com.miro.project.model.Role;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
@@ -14,4 +16,8 @@ public class LoginRequest {
     @Schema(description = "Your password", example = "jacksonLS63!+")
     @NotBlank(message = "Password is required")
     private String password;
+
+    @Schema(description = "The role you are logging in as", example = "PATIENT")
+    @NotNull(message = "Role is required to identify the account")
+    private Role role;
 }
