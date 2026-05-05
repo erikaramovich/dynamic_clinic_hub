9a3635ed88d265a0eb2265cf69590aa79514ae61
erikaramovich <erik.g@miro.com>
2026-05-06 03:56:10 +0400

    Appointment ms: init commit

---
+++ appointment-ms/src/main/java/com/miro/project/AppointmentMsApplication.java
@@ -0,0 +1,13 @@
+package com.miro.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppointmentMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppointmentMsApplication.class, args);
+	}
+
+}
