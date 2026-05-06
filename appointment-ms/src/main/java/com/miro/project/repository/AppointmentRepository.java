44bc044fb1313af3021bf639fdba65358980bbc8
erikaramovich <erik.g@miro.com>
2026-05-06 19:23:44 +0400

    Appointment ms: modify repository with pageable parameters

--- appointment-ms/src/main/java/com/miro/project/repository/AppointmentRepository.java
+++ appointment-ms/src/main/java/com/miro/project/repository/AppointmentRepository.java
@@ -2,12 +2,14 @@ package com.miro.project.repository;
 
 import com.miro.project.model.Appointment;
 import com.miro.project.model.AppointmentStatus;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
 import java.util.UUID;
 
 public interface AppointmentRepository extends JpaRepository<Appointment, UUID> {
-    List<Appointment> findAllByPatientId(UUID patientId);
-    List<Appointment> findAllByDoctorId(UUID doctorId);
-    List<Appointment> findAllByStatus(AppointmentStatus status);
+    Page<Appointment> findAllByPatientId(UUID patientId, Pageable pageable);
+    Page<Appointment> findAllByStatus(AppointmentStatus status, Pageable pageable);
+    Page<Appointment> findAll(Pageable pageable);
+    Page<Appointment> findAllByDoctorId(UUID id, Pageable pageable);
 }
