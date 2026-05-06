53c2012f43238031b59d5283ddd807cca220fb49
erikaramovich <erik.g@miro.com>
2026-05-06 19:21:56 +0400

    Appointment ms: add kafka configuration, add bootstrap-servers in dev and stand profiles

---
+++ appointment-ms/src/main/java/com/miro/project/config/KafkaConfig.java
@@ -0,0 +1,51 @@
+package com.miro.project.config;
+
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JacksonJsonSerializer;
+import org.springframework.kafka.transaction.KafkaTransactionManager;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class KafkaConfig {
+
+    @Value(value = "${spring.kafka.bootstrap-servers}")
+    private String bootstrapAddress;
+
+    // 1. Inject the transaction prefix from your application.yaml
+    @Value(value = "${kafka.producer.transaction-id-prefix}")
+    private String transactionIdPrefix;
+
+    @Bean
+    public ProducerFactory<String, Object> producerFactory(){
+        Map<String, Object> configProperties = new HashMap<>();
+        configProperties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
+        configProperties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        configProperties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JacksonJsonSerializer.class);
+
+        DefaultKafkaProducerFactory<String, Object> factory = new DefaultKafkaProducerFactory<>(configProperties);
+
+        // 2. Set the transaction ID prefix to enable transaction support
+        factory.setTransactionIdPrefix(transactionIdPrefix);
+
+        return factory;
+    }
+
+    @Bean
+    public KafkaTemplate<String, Object> kafkaTemplate(ProducerFactory<String, Object> producerFactory) {
+        return new KafkaTemplate<>(producerFactory);
+    }
+
+    @Bean
+    public KafkaTransactionManager<String, Object> kafkaTransactionManager(ProducerFactory<String, Object> producerFactory) {
+        return new KafkaTransactionManager<>(producerFactory);
+    }
+}
