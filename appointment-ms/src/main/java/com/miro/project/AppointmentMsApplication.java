b418967ea2971c963957d92d9ce47e545fb715e1
erikaramovich <erik.g@miro.com>
2026-05-08 00:58:18 +0400

    Appointment ms: manage the createdAt and updatedAt timestamps automatically by Spring Boot

--- appointment-ms/src/main/java/com/miro/project/AppointmentMsApplication.java
+++ appointment-ms/src/main/java/com/miro/project/AppointmentMsApplication.java
@@ -2,8 +2,10 @@ package com.miro.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing // REQUIRED: Enables automated auditing like @CreatedDate
 public class AppointmentMsApplication {
 
 	public static void main(String[] args) {
