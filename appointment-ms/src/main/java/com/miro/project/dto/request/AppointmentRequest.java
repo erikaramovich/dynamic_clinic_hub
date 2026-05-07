4cda4c3052c92e8d16e04c84cb1ee8d4e26adb5a
erikaramovich <erik.g@miro.com>
2026-05-08 02:41:07 +0400

    Appointment ms: change the request field from UUID to String

--- appointment-ms/src/main/java/com/miro/project/dto/request/AppointmentRequest.java
+++ appointment-ms/src/main/java/com/miro/project/dto/request/AppointmentRequest.java
@@ -4,13 +4,13 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Future;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
+
 import java.time.Instant;
-import java.util.UUID;
 
 @Data
 public class AppointmentRequest {
-    @Schema(description = "Your preferred Doctor Id or nothing", example = "5e80d829-b7ff-4e1e-a6ef-08e0e5bab429")
-    private UUID doctorId;  // Can be null for 'requested' status
+    @Schema(description = "The Name of the doctor you want to see", example = "Hovo")
+    private String doctorName;
 
     @NotNull(message = "Appointment time is required")
     @Future(message = "Appointment must be scheduled for a future date")
