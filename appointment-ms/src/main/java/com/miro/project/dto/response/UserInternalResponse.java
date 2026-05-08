543a8343c9f5d316b74289f3f20d86f2e236df80
erikaramovich <erik.g@miro.com>
2026-05-08 09:42:37 +0400

    Appointment ms: add constructor annotations

--- appointment-ms/src/main/java/com/miro/project/dto/response/UserInternalResponse.java
+++ appointment-ms/src/main/java/com/miro/project/dto/response/UserInternalResponse.java
@@ -1,13 +1,17 @@
 package com.miro.project.dto.response;
 
 import com.miro.project.model.Role;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.UUID;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserInternalResponse {
     private UUID id;
     private String name;
