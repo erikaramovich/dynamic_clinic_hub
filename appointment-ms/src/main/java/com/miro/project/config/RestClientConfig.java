6c7a679fba46c553d9b2a5c69475a0254f07d6f3
erikaramovich <erogalstyan19@gmail.com>
2026-05-26 02:15:26 +0400

    Appointment ms: configure RestClient with timeouts for authWebClient

--- appointment-ms/src/main/java/com/miro/project/config/RestClientConfig.java
+++ appointment-ms/src/main/java/com/miro/project/config/RestClientConfig.java
@@ -3,6 +3,7 @@ package com.miro.project.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 
 @Configuration
@@ -13,8 +14,13 @@ public class RestClientConfig {
 
     @Bean
     public RestClient authWebClient() {
+        SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
+        requestFactory.setConnectTimeout(2000);
+        requestFactory.setReadTimeout(5000);
+
         return RestClient.builder()
                 .baseUrl(authServiceUrl)
+                .requestFactory(requestFactory)
                 .build();
     }
 }
