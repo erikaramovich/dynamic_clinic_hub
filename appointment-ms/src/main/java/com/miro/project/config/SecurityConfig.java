108b75d13b79a929e96794ae9ed969b19fa09bfe
erikaramovich <erogalstyan19@gmail.com>
2026-06-10 00:25:37 +0400

    Appointment ms: add new allowed frontend and other backend origins for CORS configuration

--- appointment-ms/src/main/java/com/miro/project/config/SecurityConfig.java
+++ appointment-ms/src/main/java/com/miro/project/config/SecurityConfig.java
@@ -24,7 +24,7 @@ public class SecurityConfig {
     private final JwtAuthenticationFilter jwtFilter;
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain filterChain(HttpSecurity http) {
         http.cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
@@ -38,7 +38,14 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:63343", "http://localhost:8080"));
+        configuration.setAllowedOrigins(List.of(
+                "http://localhost:63343",
+                "http://127.0.0.1:63343",
+                "http://localhost:5500",
+                "http://127.0.0.1:5500",
+                "http://localhost:3000",
+                "http://localhost:8080"
+        ));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true);
